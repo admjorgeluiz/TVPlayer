3fa294de7e508d19832f695f2ebdedcb29e8cfd5
admjorgeluiz <jorgeluiz.adm@outlook.com>
2025-07-11 18:26:27 -0300

    Att gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     id("kotlin-parcelize")
 
-    // SR_NOTE: Se for usar o glide-compiler, adicione o plugin kotlin-kapt aqui
-    // alias(libs.plugins.kotlin.kapt) // Supondo que você defina kotlin-kapt no [plugins] do TOML
 }
 
 android {
@@ -15,8 +13,8 @@ android {
         applicationId = "com.jorgenascimento.tvplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -45,27 +43,25 @@ android {
 }
 
 dependencies {
-    implementation(libs.kotlin.stdlib) // Alias: kotlin-stdlib
+    implementation(libs.kotlin.stdlib)
 
-    implementation(libs.androidx.core.ktx) // Alias: androidx-core-ktx
+    implementation(libs.androidx.core.ktx)
 
-    implementation(libs.androidx.appcompat) // Alias: androidx-appcompat
-    implementation(libs.google.material)    // SR_CORRECTION: Alias correto é googleMaterial (do google-material no TOML)
-    implementation(libs.androidx.activity.ktx) // SR_CORRECTION: Alias correto é androidxActivityKtx (do androidx-activity-ktx no TOML)
-    implementation(libs.androidx.constraintlayout) // Alias: androidx-constraintlayout
-    implementation(libs.androidx.recyclerview)   // Alias: androidx-recyclerview
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.material)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
 
-    implementation(libs.libvlc.all)      // SR_CORRECTION: Alias correto é libvlcAll (do libvlc-all no TOML)
+    implementation(libs.libvlc.all)
 
-    implementation(libs.gson)                 // Alias: gson
-    implementation(libs.play.services.ads)  // SR_CORRECTION: Alias correto é playServicesAds (do play-services-ads no TOML)
+    implementation(libs.gson)
+    implementation(libs.play.services.ads)
 
-    implementation(libs.glide)                // Alias: glide
-    // SR_NOTE: Se usar o glide-compiler, descomente a linha abaixo e adicione o plugin kapt
-    // kapt(libs.glide.compiler)          // Supondo um alias glide-compiler no TOML
+    implementation(libs.glide)
 
-    testImplementation(libs.junit)                              // Alias: junit
-    androidTestImplementation(libs.androidx.junit.ext)        // SR_CORRECTION: Alias correto é androidxJunitExt (do androidx-junit-ext no TOML)
-    androidTestImplementation(libs.androidx.espresso.core)    // SR_CORRECTION: Alias correto é androidxEspressoCore (do androidx-espresso-core no TOML)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit.ext)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
 
