411b3154e99375b318a28c215168477a811e4ca1
admjorgeluiz <jorgeluiz.adm@outlook.com>
2025-08-09 17:03:05 -0300

    Atualização jvm(build.gradle)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         viewBinding = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
 
